6481a16c0384a63821958c4d803b85cf27c3fb06
Roberto Polli <robipolli@gmail.com>
2018-05-25 18:21:54 +0200

    Implement service and fix error in generator.

---
+++ prj-simple/swagger_server/test/test_public_controller.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.problem import Problem  # noqa: E501
+from swagger_server.models.timestampa import Timestampa  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestPublicController(BaseTestCase):
+    """PublicController integration test stubs"""
+
+    def test_get_echo(self):
+        """Test case for get_echo
+
+        Ritorna un timestamp in formato RFC5424.
+        """
+        response = self.client.open(
+            '/datetime/v1/echo',
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
